a092a3fa15dd45e48e7520aaa56eac7eb8127b82
Daniel Planoetscher <d.planoetscher@kreatif.it>
2022-09-30 21:43:40 +0200

    article serializer improved: now with module key

--- plugins/structure/lib/serializers/ArticleSliceSerializer.php
+++ plugins/structure/lib/serializers/ArticleSliceSerializer.php
@@ -14,15 +14,22 @@ class ArticleSliceSerializer extends Serializer
     public function toArray($object): array
     {
         $result['id'] = $object->getId();
-        $result['module'] = $object->getModuleId();
+        $result['module'] = $this->getModule($object->getModuleId());
         for ($i = 1; $i <= 20; $i++) {
             $this->addValueToResult($result, 'values', $i, $object->getValue($i));
+        }
+        for($i = 1; $i <= 10; $i++) {
             $this->addValueToResult($result, 'media', $i, $object->getMedia($i));
             $this->addValueToResult($result, 'mediaList', $i, $object->getMediaList($i), true);
             $this->addValueToResult($result, 'link', $i, $object->getLink($i));
             $this->addValueToResult($result, 'linkList', $i, $object->getLinkList($i), true);
         }
         return $result;
+    }
+
+    private function getModule($moduleId) {
+        $modules = rex_file::getCache(rex_module_cache::getKeyMappingPath());
+        return $modules[$moduleId] ?: $moduleId;
     }
 
     private function addValueToResult(array &$result, string $type, int $idx, $value, bool $isList = false)
